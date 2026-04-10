acc60cbcf86c3abc67b50cdfc5e458b106e1fe66
Ruben <rubencarreteropelaez@gmail.com>
2026-04-10 20:30:44 +0200

    Prueba Undefined API_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,10 @@
 const API_URL = process.env.REACT_APP_API_URL
 
+console.log("API_URL:", API_URL);
+
 export const login = async (data) => {
   try {
-    const res = await fetch(`${API_URL}/auth/login`, {
+    const res = await fetch(`${API_URL}/api/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
